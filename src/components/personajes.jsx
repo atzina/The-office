ed72fd8881b06d221448ab8731191ea2a524568d
atzina <atzina@gmail.com>
2023-04-19 23:19:40 -0600

    intento de consumo de apiII

--- src/components/personajes.jsx
+++ src/components/personajes.jsx
@@ -3,17 +3,6 @@ function Personajes () {
     const [personajes, setPersonajes] = useState(null);
     useEffect(function getPersonajes() {
         
-        const options = {
-            metod:'GET',
-            headers: {
-                'X-RapidAPI-Key': '6ffa894d07msh750bf1c53f05377p1abb9djsn5df8cb14612a',
-		'X-RapidAPI-Host': 'the-office.p.rapidapi.com'
-            }
-        }
-
-        fetch('https://the-office.p.rapidapi.com/1', options)
-        .then(response => response.json())
-        .then(data => console.log(data))
 	    
     }
     , []);
